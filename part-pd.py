9e1008ccc47af4e60f7669b6dbabd90b0171e175
Gilang Ardya <gilangardya@yahoo.com>
2017-05-19 05:25:42 +0700

    Create part-pd.py

---
+++ part-pd.py
@@ -0,0 +1,24 @@
+# Penyelesaian persoalan Partisi Bilangan bulat dengan pemrograman dinamis
+import time
+
+def partition(n):
+	tabel = [[0 for x in range(n)] for y in range(n+1)]
+	for i in range(n+1):
+		for j in range(n):
+			if (i < 2 or j < 1):
+				tabel[i][j] = 1
+			else:
+				tamp = 0
+				brs = i-1
+				kol = 0
+				while (brs >= 0 and kol <= j):
+					tamp += tabel[brs][kol]
+					brs -= 1
+					kol += 1
+				tabel[i][j] = tamp
+	return tabel[n][n-1]
+
+start = time.time()
+print(partition(400))
+end = time.time()
+print((end - start)*1000)
