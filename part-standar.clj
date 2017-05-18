ad200ccc65f89179af4b5705032086c0d2e77cb1
Gilang Ardya <gilangardya@yahoo.com>
2017-05-19 05:18:04 +0700

    Create part-standar.clj

---
+++ part-standar.clj
@@ -0,0 +1,10 @@
+; Penyelesaian persoalan Partisi Bilangan bulat dengan relasi rekurens standar
+
+(defn part [[a b]]
+    (cond 
+        (or (< a 0) (< b 1)) 0
+        (or (< a 2) (= b 1)) 1
+        :else (+' (part [(- a b) b]) (part [a (dec b)]))))
+(defn integerPartition [n] (part [n n]))
+
+(time (println (integerPartition 70)))
